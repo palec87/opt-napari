013d39fab670c3ad7880ed6bde6632afc0076b0f
palec87 <david@stanka.de>
2024-04-16 19:10:47 +0100

    adding tests and test cases

--- src_all/test/test_corr.py
+++ src_all/test/test_corr.py
@@ -13,14 +13,29 @@ __author__ = 'David Palecek'
 __credits__ = ['Teresa M Correia', 'Giorgia Tortora']
 __license__ = 'GPL'
 
-hot1 = np.ones((5, 5))
+hot1 = dark1 = bright1 = np.ones((5, 5))
+# define hot one as a copy of hot1 with one pixel changed
+hot2 = hot1.copy()
+hot2[2, 1] = 10
+
+# define a test image
+img1 = np.ones((5, 5)) * 5
+img2 = img1.copy()
+img2[2, 1] = 1
+
+img3 = np.ones((5, 5)) * 3
+img4 = np.ones((5, 5)) * 4
+
+stack1 = np.ones((5, 5, 5)) * 5
 
 
 @pytest.mark.parametrize(
     'init_vals, expected',
-    [((), (None, 7, None, None)),
+    [((), (None, 7, None, None)),  # no xorrections
      ((None, ), (None, 7, None, None)),
      ((hot1, ), ([], 7, None, None)),
+     ((hot2, 5, ), ([], 5, None, None)),  # with five the cutoff is 10.17
+     ((hot2, 4.85, ), ([(2, 1)], 4.85, None, None)),
      ],
 )
 def test_init(init_vals, expected):
@@ -29,3 +44,63 @@ def test_init(init_vals, expected):
     assert corr.std_mult == expected[1]
     assert corr.dark is expected[2]
     assert corr.bright_corr is expected[3]
+
+
+@pytest.mark.parametrize(
+    'inits, img, expected',
+    [((hot1, ), img1, img1),
+     ((hot2, 5, ), img1, img1),
+     ((hot2, 4, ), img1, img1),  # this does not do anything, because img is flat
+     ((hot2, 4, ), img2, img1),  # this is correctimg stuff
+     ],
+)
+def test_correct_hot(inits, img, expected):
+    corr = Correct(*inits)
+    corrected = corr.correct_hot(img)
+    assert corrected.all() == expected.all()
+
+
+@pytest.mark.parametrize(
+    'inits, img, expected',
+    [((hot2, 4, dark1), hot1, np.zeros((5, 5))),  # two same arrays
+     ((hot2, 4, dark1), img2, img2 - dark1),
+     ],
+)
+def test_correct_dark(inits, img, expected):
+    corr = Correct(*inits)
+    corrected = corr.correct_dark(img)
+    assert corrected.all() == expected.all()
+
+
+@pytest.mark.parametrize(
+    'inits, img, expected',
+    [((hot2, 4, dark1, bright1), img1, img4),
+     ],
+)
+def test_correct_bright(inits, img, expected):
+    corr = Correct(*inits)
+    corrected = corr.correct_bright(img)
+    assert corrected.all() == expected.all()
+
+# TODO: check this, I think this should not pass
+@pytest.mark.parametrize(
+    'inits, img, expected',
+    [((hot2, 4, dark1, bright1), img1, img3),  # dark =1, bright = 1, img1 = 5
+     ],
+)
+def test_correct_bright2(inits, img, expected):
+    corr = Correct(*inits)
+    corrected = corr.correct_dark(img)
+    corrected = corr.correct_bright(corrected)
+    assert corrected.all() == expected.all()
+
+
+@pytest.mark.parametrize(
+    'inits, corr_int_inputs, expected',
+    [((hot2, 4, dark1, bright1), (stack1,), img1),
+     ],
+)
+def test_correct_int(inits, corr_int_inputs, expected):
+    corr = Correct(*inits)
+    corrected = corr.correct_int(*corr_int_inputs)
+    assert corrected.all() == expected.all()
--- src_all/test/test_utils.py
+++ src_all/test/test_utils.py
@@ -2,6 +2,9 @@
 
 """
 Testing the low level helper functions
+TODO: fix binning based on tests
+TODO: add test cases, especially edge cases
+TODO: functional tests of all corrections in combinations, closer to real life.
 """
 
 import pytest
@@ -16,10 +19,12 @@ __credits__ = ['Teresa M Correia', 'Giorgia Tortora']
 __license__ = 'GPL'
 
 
-# TODO: more test cases
 @pytest.mark.parametrize(
     'arr_in, ULCorner, height, width, arr_out, roi_out',
     [(np.ones((5, 5)), (2, 2), 2, 2, np.ones((2, 2)), (2, 4, 2, 4)),
+     (np.ones((5, 5)), (2, 2), 3, 3, np.ones((3, 3)), (2, 5, 2, 5)),
+     (np.ones((5, 5)), (2, 2), 4, 4, np.ones((3, 3)), (2, 5, 2, 5)),  # ROI beyond the image
+     (np.ones((5, 5)), (2, 2), 0, 0, np.ones((0, 0)), (2, 2, 2, 2)),  # empty ROI
      ],
 )
 def test_select_roi(arr_in, ULCorner, height, width, arr_out, roi_out):
@@ -29,8 +34,22 @@ def test_select_roi(arr_in, ULCorner, height, width, arr_out, roi_out):
 
 
 @pytest.mark.parametrize(
+    'arr_in, ULCorner, height, width, out',
+    [(np.ones((5, 5)), (2, 2), -1, -1, None),  # this should throw an error
+     ],
+)
+def test_select_roi2(arr_in, ULCorner, height, width, out):
+    assert select_roi(arr_in, ULCorner, height, width) == out
+
+
+# this is failing for unmatched dimensions,
+@pytest.mark.parametrize(
     'arr_in, bin_factor, expected',
     [(np.ones((5, 4, 4)), 2, np.ones((5, 2, 2))),
+     (np.ones((5, 4, 4)), 3, np.ones((5, 1, 1))),
+     (np.ones((5, 5, 5)), 3, np.ones((5, 1, 1))),
+     (np.ones((5, 6, 6)), 3, np.ones((5, 2, 2))),
+     (np.ones((5, 6, 6)), 2, np.ones((5, 3, 3))),
      ],
     )
 def test_bin_3d(arr_in, bin_factor, expected):
@@ -41,16 +60,21 @@ def test_bin_3d(arr_in, bin_factor, expected):
 @pytest.mark.parametrize(
     'arr_in, ret_type, expected',
     [(2 * np.ones((4, 4)), 'float', np.ones((4, 4))),
+     (2 * np.ones((4, 4)), 'int', np.ones((4, 4))),
+     (2.5 * np.ones((4, 4)), 'float', np.ones((4, 4))),
      ],
     )
 def test_norm_img(arr_in, ret_type, expected):
     out = norm_img(arr_in, ret_type)
+    assert out.dtype == ret_type
     np.testing.assert_array_equal(out, expected)
 
 
 @pytest.mark.parametrize(
     'arr_in, dtype, expected',
     [(np.ones((4, 4)), np.uint8, np.ones((4, 4))),
+     (np.ones((4, 4)) * 255, np.uint8, np.ones((4, 4)) * 255),
+     (np.ones((4, 4)) * 256, np.uint8, np.ones((4, 4)) * 255),
      ],
     )
 def test_img2intType(arr_in, dtype, expected):
@@ -59,10 +83,12 @@ def test_img2intType(arr_in, dtype, expected):
 
 
 @pytest.mark.parametrize(
-    'arr_in, corr_type, expected',
-    [(np.ones((4, 4)), 'dark_corr', 0),
+    'inputs, expected',
+    [((np.ones((4, 4)), 'dark_corr'), 0),
+     ((np.ones((4, 4)) - 2, 'dark_corr'), 1),
+     ((np.ones((4, 4)) - 2, ), 1),
      ],
     )
-def testIsPositive(arr_in, corr_type, expected):
-    out = is_positive(arr_in, corr_type)
+def testIsPositive(inputs, expected):
+    out = is_positive(*inputs)
     assert out == expected
