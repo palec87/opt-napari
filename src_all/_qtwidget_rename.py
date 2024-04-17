cc483ef13e2b1b422573466a22a90e85758e8750
GiorgiaTortora <giorgiatortora2@gmail.com>
2024-04-17 23:41:42 +0200

    changes to qt_widget

--- src_all/_qtwidget_rename.py
+++ src_all/_qtwidget_rename.py
@@ -1,4 +1,4 @@
-# import numpy as np
+import numpy as np
 # from napari import Viewer
 # from typing import Annotated
 # import os
@@ -11,7 +11,7 @@
 # import scipy.ndimage as ndi
 # import cv2 
 # import tqdm
-# from corrections import Correct
+from corrections import Correct
 # from dataclasses import dataclass
 from napari.viewer import current_viewer
 from typing import List
@@ -20,7 +20,7 @@ from functools import partial
 from napari.layers import Layer, Image, Points
 from magicgui import magic_factory
 from magicgui.widgets import create_widget
-from napari.utils import notifications
+from napari.utils import progress, notifications
 import warnings
 from widget_settings import Settings, Combo_box
 import napari
@@ -38,10 +38,7 @@ from utils import (
     bin_3d,
 )
 
-ID_NAME = "_CLUSTER_ID"
-
-
-def layer_container_and_selection(viewer=None):
+def layer_container_and_selection(viewer=None, layer_type = Image, container_name = 'Layer'):
     """
     Create a container and a dropdown widget to select the layer.
 
@@ -52,8 +49,8 @@ def layer_container_and_selection(viewer=None):
     """
     layer_selection_container = QWidget()
     layer_selection_container.setLayout(QHBoxLayout())
-    layer_selection_container.layout().addWidget(QLabel("Layer"))
-    layer_select = create_widget(annotation=Layer, label="layer")
+    layer_selection_container.layout().addWidget(QLabel(container_name))
+    layer_select = create_widget(annotation= layer_type, label="layer")
     layer_selection_container.layout().addWidget(layer_select.native)
 
     if viewer is not None and viewer.layers.selection.active is not None:
@@ -78,118 +75,78 @@ class PreprocessingnWidget(QWidget):
         self.setup_ui()
     
     def setup_ui(self):
+        
         # initialize layout
         layout = QVBoxLayout()
         self.setLayout(layout)
-
-        (
-            layer_selection_container,
-            self.layer_select,
-        ) = layer_container_and_selection(viewer=self.viewer)
-
-        # update axes combo boxes automatically if features of
-        # layer are changed
-        self.last_connected = None
-        self.layer_select.changed.connect(self.activate_property_autoupdate)
         
-        image_layout = QSplitter(Qt.Vertical)#QVBoxLayout()
-        layout.addWidget(image_layout)
-        self.layout().addWidget(layer_selection_container)
-        # add_section(image_layout, 'Image selection')
-        # layout.addLayout(image_layout)
-        
-        # self.multiple_layers_widget = multiple_layers()
-        # self.multiple_layers_widget.call_button.visible = False
-        # self.add_magic_function(self.multiple_layers_widget, image_layout)
-        # select_button = QPushButton('Select layers')
-        # select_button.clicked.connect(self.select_layers)
-        # image_layout.addWidget(select_button)
-        
-        # self.choose_layer_widget = choose_layer()
-        # self.choose_layer_widget.call_button.visible = False
-        # self.add_magic_function(self.choose_layer_widget, image_layout)
-        # select_button = QPushButton('Select image layer')
-        # select_button.clicked.connect(self.select_layer)
-        # image_layout.addWidget(select_button)
+        mode_layout = QVBoxLayout()
+        layout.addLayout(mode_layout)
+        self.choose_mode(mode_layout)
 
-        # self.selectImageLayerWidget = create_widget(annotation=Image,
-        #                                             label="Image_layer",
-        #                                             )
-        # image_layout.addWidget(self.selectImageLayerWidget.native)
-        # self.installEventFilter(self)
-
-        # self.choose_points_layer_widget = choose_points_layer()
-        # self.choose_points_layer_widget.call_button.visible = False
-        # self.add_magic_function(self.choose_points_layer_widget, image_layout)
-        # select_points_button = QPushButton('Select points layer')
-        # select_points_button.clicked.connect(self.select_points_layer)
-        # image_layout.addWidget(select_points_button)
+        (image_selection_container, 
+         self.image_layer_select) = layer_container_and_selection(viewer=self.viewer, 
+                                                                  layer_type = Image, 
+                                                                  container_name = 'Image to analyze')
+        (hot_image_selection_container, 
+         self.hot_layer_select) = layer_container_and_selection(viewer=self.viewer, 
+                                                                layer_type = Image, 
+                                                                container_name = 'Hot correction image')
+        (dark_image_selection_container, 
+         self.dark_layer_select) = layer_container_and_selection(viewer=self.viewer, 
+                                                                 layer_type = Image, 
+                                                                 container_name = 'Dark correction image')
+        (bright_image_selection_container, 
+         self.bright_layer_select) = layer_container_and_selection(viewer=self.viewer, 
+                                                                   layer_type = Image, 
+                                                                   container_name = 'Bright correction image')
+        (points_selection_container, 
+         self.points_layer_select) = layer_container_and_selection(viewer=self.viewer, 
+                                                                   layer_type = Points, 
+                                                                   container_name = 'Points layer for ROI selection')
+        
+        image_layout = QVBoxLayout()
+        layout.addLayout(image_layout)
+        self.layout().addWidget(image_selection_container)
+        self.layout().addWidget(hot_image_selection_container)
+        self.layout().addWidget(dark_image_selection_container)
+        self.layout().addWidget(bright_image_selection_container)
+        self.layout().addWidget(points_selection_container)
+        
+        for container in [image_selection_container, hot_image_selection_container,
+                      dark_image_selection_container, bright_image_selection_container,
+                      points_selection_container]:
+            container.layout().setContentsMargins(0, 0, 0, 10)
 
         settings_layout = QVBoxLayout()
         layout.addLayout(settings_layout)
         self.createSettings(settings_layout)
-
-    def activate_property_autoupdate(self):
-        if self.last_connected is not None:
-            self.last_connected.events.properties.disconnect(
-                partial(update_properties_list, self, [ID_NAME])
-            )
-        self.layer_select.value.events.properties.connect(
-            partial(update_properties_list, self, [ID_NAME])
-        )
-        self.last_connected = self.layer_select.value
 
     def showEvent(self, event) -> None:
         super().showEvent(event)
         self.reset_choices()
 
     def reset_choices(self, event=None):
-        self.layer_select.reset_choices(event)
-            
-    def select_layer(self, image: Image):
-
-        image = self.choose_layer_widget.image.value
-
-        if image.data.ndim == 3:
-
-            self.imageRaw_name = image.name
-            sz,sy,sx = image.data.shape
-            # print(sz, sy, sx)
-            # if not hasattr(self, 'h'): 
-            #     self.start_preprocessing()
-            print(f'Selected image: {image.name}')
-
-    def select_layers(self, x: List[Layer]):
-        print(x)
-        names = []
-        # layers_list = self.example_widget.x.value
-        # layers_list = x
-
-        for layer in x:
-            # self._layer_name = layer.name
-            names.append(layer.name)
-            print(f'Selected layer: {layer.name}')
-        print(names)
-        return names
-    
-    def select_points_layer(self,       ##TODO it is not correct like this, we need one function that collects all layers
-                     points: Points):
-    
-        points = self.choose_points_layer_widget.points.value
-        self._points_name = points.name
-        print(f'Selected points: {points.name}')
-
-    def createSettings(self, slayout):
+        self.image_layer_select.reset_choices(event)
+        self.hot_layer_select.reset_choices(event)
+        self.dark_layer_select.reset_choices(event)
+        self.bright_layer_select.reset_choices(event)
+        self.points_layer_select.reset_choices(event)
         
-        layout = QVBoxLayout() ##TODO change layout, put the 2 checkboxes one near to the other
+    def choose_mode(self,slayout):
         
         self.inplace = Settings('Inplace operations', dtype=bool, initial=False,
                                       layout=slayout, 
                                       write_function = self.set_preprocessing)
         
-        self.track = Settings('Track', dtype=bool, initial=False,
+        self.track = Settings('Track', dtype=bool, initial=False, #TODO track box must appear only when inplace box=True
                                       layout=slayout,
                                       write_function = self.set_preprocessing)
+        
+    def createSettings(self, slayout):
+        
+        layout = QVBoxLayout()
+        
         
         self.std_cutoff = Settings('Std cutoff',
                                   dtype=int, 
@@ -221,12 +178,12 @@ class PreprocessingnWidget(QWidget):
                                   layout=slayout, 
                                   write_function = self.set_preprocessing)    
         
-    #     # self.listlayers = Combo_box(name = 'Layer_lit', choices = Accel.available(),
-    #     #                           layout=right_layout,
-    #     #                           write_function = self.setReconstructor)    
         # buttons
         buttons_dict = {'Select ROI': self.select_ROIs,
-                        'Binning': self.bin_stack
+                        'Binning': self.bin_stack,
+                        'Hot pixel correction': self.correct_hot_pixels,
+                        'Dark-field correction': self.correct_dark,
+                        'Bright-field correction': self.correct_bright
                         }
         for button_name, call_function in buttons_dict.items():
             button = QPushButton(button_name)
@@ -235,19 +192,6 @@ class PreprocessingnWidget(QWidget):
         self.messageBox = QLineEdit()
         layout.addWidget(self.messageBox, stretch=True)
         self.messageBox.setText('Messages') #not working, I think
-        
-    
-    def get_image(self):
-        try:
-            return self.viewer.layers[self.imageRaw_name].data
-        except:
-             raise(KeyError(r'Please select a valid 3D image ($\theta$, q, z)'))
-             
-    def get_points(self): ##TODO it would be good to have just one function to select all layers
-        try:
-            return self.viewer.layers[self._points_name].data
-        except:
-             raise(KeyError(r'Please select a valid Points layer'))
              
     def show_image(self, image_values, _inplace_value, fullname):
         
@@ -256,18 +200,16 @@ class PreprocessingnWidget(QWidget):
         # else:
         #     scale = [1.]*image_values.ndim
         
-        if _inplace_value == True:   ##TODO consider to add option to update existing layer
-            fullname = self.imageRaw_name
-            
+        if _inplace_value == True:
+            fullname = self.image_layer_select.value.name
             self.viewer.layers[fullname].data = image_values
             # self.viewer.layers[fullname].scale = scale
-        
         else:  
             layer = self.viewer.add_image(image_values,
                                             name = fullname,
                                             # scale = scale,
                                             interpolation2d = 'linear')
-            return layer
+        return layer
     
     def set_preprocessing(self, *args):
         '''
@@ -286,10 +228,11 @@ class PreprocessingnWidget(QWidget):
             # self.h.set_reconstruction_process()
             
     #select ROIs    
-    def select_ROIs(self): ##TODO add tracking option for select_ROIs and bin_stack
+    def select_ROIs(self):
  
-        original_image = self.get_image()
-        points = self.get_points()
+        original_image = self.image_layer_select.value.data
+        points = self.points_layer_select.value.data
+        self.imageRaw_name = self.image_layer_select.value.name
         _inplace_value = self.inplace.val
         fullname = 'ROI_' + self.imageRaw_name
         notifications.show_info(f'UL corner coordinates: {points[0][1:].astype(int)}')
@@ -299,28 +242,92 @@ class PreprocessingnWidget(QWidget):
     #binning
     def bin_stack(self):
         if self.bin_factor != 1:
-            original_image = self.get_image()
+            original_image = self.image_layer_select.value.data
+            self.imageRaw_name = self.image_layer_select.value.name
             _inplace_value = self.inplace.val
-            fullname = 'Binned_' + self.imageRaw_name
+            fullname = 'binned_' + self.imageRaw_name 
             binned_roi = bin_3d(original_image, self.bin_factor.val)
             notifications.show_info(f'Original shape: {original_image.shape}, binned shape: {binned_roi.shape}')
             self.show_image(binned_roi, _inplace_value, fullname)
         else:
             notifications.show_info('Bin factor is 1, nothing to do.')
-                
-# def current_layers(_):
-#     return list(current_viewer().layers)
-
-# @magic_factory(x={'widget_type': 'Select', "choices": current_layers})
-# def multiple_layers(x: List[Layer]):
-#     pass
+    
+    #hot pixels correction (NOT WORKING YET)
+    def correct_hot_pixels(self):
+ 
+        original_image = self.image_layer_select.value.data
+        hot_pixels_image = self.hot_layer_select.value.data
+        self.imageRaw_name = self.image_layer_select.value.name
+        _inplace_value = self.inplace.val
+        _std_cutoff = self.std_cutoff.val
+        fullname = 'Hot correction' + self.imageRaw_name
         
-# @magic_factory
-# def choose_layer(image: Image):
-#         pass #TODO: substitute with a qtwidget without magic functions
-# @magic_factory
-# def choose_points_layer(points: Points):
-#         pass #TODO: substitute with a qtwidget without magic functions
+        # init correction class
+        corr = Correct(hot=hot_pixels_image, std_mult=_std_cutoff, dark=None, bright=None)
+        # preallocate corrected array
+        data_corr = np.zeros(original_image.shape,
+                              dtype=original_image.dtype)
+        print(f'number of hot pixels: {len(corr.hot_pxs)}')
+
+        # Bad pixels
+        print('to be implemented')
+    
+        # # TODO: intensity correction not implemented!!!
+        # # now updating the image.
+        # if history.inplace:
+        #     new_data = {'operation': 'correct',
+        #                 'data': data_corr,
+        #                 }
+        #     image.data = history.update_history(image, new_data)
+        # else:
+        #     viewer.add_image(data_corr)
+        # print('debug corrections', history.roi_def)
+        
+        # self.show_image(selected_roi, _inplace_value, fullname)
+        self.show_image(data_corr, _inplace_value, fullname)
+        
+    #dark-field correction  (WORKS CORRECTLY WITH INPLACE BUT NOT WITH NOINPLACE)
+    def correct_dark(self):
+ 
+        original_image = self.image_layer_select.value.data
+        dark_image = self.dark_layer_select.value.data
+        self.imageRaw_name = self.image_layer_select.value.name
+        _inplace_value = self.inplace.val
+        fullname = 'dark_correction _' + self.imageRaw_name
+        
+        # init correction class
+        corr = Correct(hot=None, std_mult=None, dark=dark_image, bright=None)
+        # preallocate corrected array
+        data_corr = np.zeros(original_image.shape,
+                             dtype=original_image.dtype)
+
+        for i, img in progress(enumerate(original_image)):
+            data_corr[i] = corr.correct_dark(img)
+        print(f'max: {np.amax(data_corr)}, min: {np.amin(data_corr)}')
+        notifications.show_info('Dark correction done.')
+        
+        self.show_image(data_corr, _inplace_value, fullname)
+        
+    #bright-field correction
+    def correct_bright(self):
+ 
+        original_image = self.image_layer_select.value.data
+        bright_image = self.bright_layer_select.value.data
+        self.imageRaw_name = self.image_layer_select.value.name
+        _inplace_value = self.inplace.val
+        fullname = 'bright_correction_' + self.imageRaw_name
+        
+        # init correction class
+        corr = Correct(hot=None, std_mult=None, dark=None, bright=bright_image)
+        # preallocate corrected array
+        data_corr = np.zeros(original_image.shape,
+                             dtype=original_image.dtype)
+
+        for i, img in progress(enumerate(data_corr)):
+            data_corr[i] = corr.correct_bright(img)
+        notifications.show_info('Bright correction done.')
+        
+        self.show_image(data_corr, _inplace_value, fullname)
 
 if __name__ == '__main__':
     import napari
